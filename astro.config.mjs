48fa25e5c9b0411c4f0a293e54428a330fb3b949
Basil Alti <basilng.bng@gmail.com>
2026-07-30 03:08:53 +0530

    chore: finalise Vercel deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://your-project.vercel.app",
   output: "static",
   trailingSlash: "always",
   compressHTML: true,
